f7db9ccc56b6d4d2982f822d16b5fdfc18476df9
egan <eganjaya75@gmail.com>
2021-06-10 00:10:52 +0800

    update PWA

---
+++ js/lib.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+
+    //pwa
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/serviceW.js')
+        .then(function(registration) {
+          console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function(error) {
+          console.log('Service worker registration failed, error:', error);
+        });
+      }
+})
---
+++ serviceW.js
@@ -0,0 +1,65 @@
+const filesToCache = [
+    '/index.html',
+    '/assets/js/main.js',
+    '/assets/css/style.css'
+  ];
+  
+  const staticCacheName = 'pages-cache-v1';
+  
+  self.addEventListener('install', event => {
+    console.log('Instal service worker');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+  self.addEventListener('activate', event => {
+    console.log('Activating new service worker...');
+  
+    //const cacheAllowlist = [staticCacheName];
+  
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+          return Promise.all(
+            cacheNames.filter(function(cacheName){
+                return cacheName!= staticCacheName;
+            }).map(function(cacheName){
+                return caches.delete(cacheName);
+            })
+          );
+        // return Promise.all(
+        //   cacheNames.map(cacheName => {
+        //     if (cacheAllowlist.indexOf(cacheName) === -1) {
+        //       return caches.delete(cacheName);
+        //     }
+        //   })
+        // );
+      })
+    );
+  });
+
+  self.addEventListener('fetch', event => {
+    console.log('isi event request :'+event.request)
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+  
+        // TODO 4 - Add fetched files to the cache
+  
+      }).catch(error => {
+  
+        // TODO 6 - Respond with custom offline page
+  
+      })
+    );
+  });
